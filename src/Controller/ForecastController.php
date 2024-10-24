fff855d285de9353b9bfaf60e1d041c7a9271666
Tomasz Woźnicki <wt51154>
2024-10-24 09:53:02 +0200

    lab-e niedokonczone

--- src/Controller/ForecastController.php
+++ src/Controller/ForecastController.php
@@ -10,11 +10,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/forecast')]
 final class ForecastController extends AbstractController
 {
     #[Route(name: 'app_forecast_index', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_INDEX')]
     public function index(ForecastRepository $forecastRepository): Response
     {
         return $this->render('forecast/index.html.twig', [
@@ -23,6 +25,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/new', name: 'app_forecast_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $forecast = new Forecast();
@@ -45,6 +48,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_forecast_show', methods: ['GET'])]
+    #[IsGranted('ROLE_FORECAST_SHOW')]
     public function show(Forecast $forecast): Response
     {
         return $this->render('forecast/show.html.twig', [
@@ -53,6 +57,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_forecast_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_FORECAST_EDIT')]
     public function edit(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(ForecastType::class, $forecast, [
@@ -73,6 +78,7 @@ final class ForecastController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_forecast_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_FORECAST_DELETE')]
     public function delete(Request $request, Forecast $forecast, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$forecast->getId(), $request->getPayload()->getString('_token'))) {
--- src/Controller/LocationController.php
+++ src/Controller/LocationController.php
@@ -10,11 +10,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 #[Route('/location')]
 final class LocationController extends AbstractController
 {
     #[Route(name: 'app_location_index', methods: ['GET'])]
+    #[IsGranted('ROLE_ADMIN')]
     public function index(LocationRepository $locationRepository): Response
     {
         return $this->render('location/index.html.twig', [
@@ -23,6 +25,7 @@ final class LocationController extends AbstractController
     }
 
     #[Route('/new', name: 'app_location_new', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_LOCATION_NEW')]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $location = new Location();
@@ -45,6 +48,7 @@ final class LocationController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_location_show', methods: ['GET'])]
+    #[IsGranted('ROLE_LOCATION_SHOW')]
     public function show(Location $location): Response
     {
         return $this->render('location/show.html.twig', [
@@ -53,6 +57,7 @@ final class LocationController extends AbstractController
     }
 
     #[Route('/{id}/edit', name: 'app_location_edit', methods: ['GET', 'POST'])]
+    #[IsGranted('ROLE_LOCATION_EDIT')]
     public function edit(Request $request, Location $location, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(LocationType::class, $location, [
@@ -73,6 +78,7 @@ final class LocationController extends AbstractController
     }
 
     #[Route('/{id}', name: 'app_location_delete', methods: ['POST'])]
+    #[IsGranted('ROLE_LOCATION_DELETE')]
     public function delete(Request $request, Location $location, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$location->getId(), $request->getPayload()->getString('_token'))) {
